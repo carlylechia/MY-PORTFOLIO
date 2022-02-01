0b60e15f702cde2f3769968c23afb3121dec7f5b
Christianib003 <christianib003@gmail.com>
2022-02-01 14:17:56 +0200

    Add Mobile pop up window

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 
-let logo = document.getElementsByClassName('logo');
-let dropDownContainer = document.getElementsByClassName('mobile-dropdown')[0];
-let menuLinks = document.querySelectorAll('drop-option');
-let icon = document.querySelector('.menu .fa-bars');
+// let logo = document.getElementsByClassName('logo');
+// let dropDownContainer = document.getElementsByClassName('mobile-dropdown')[0];
+// let menuLinks = document.querySelectorAll('drop-option');
+// let icon = document.querySelector('.menu .fa-bars');
 
-dropDownContainer.style.visibility = 'hidden';
+// dropDownContainer.style.visibility = 'hidden';
 
 
- function changeIcon() {
-   icon.classList.toggle("fa-times");
-   icon.classList.toggle('fa-bars');
-   icon.style.position = 'absolute';
-   icon.style.right = '0';
-   icon.style.marginRight = '24px';
+//  function changeIcon() {
+//    icon.classList.toggle("fa-times");
+//    icon.classList.toggle('fa-bars');
+//    icon.style.position = 'absolute';
+//    icon.style.right = '0';
+//    icon.style.marginRight = '24px';
 
-   if (dropDownContainer.style.visibility === 'hidden') {
-    dropDownContainer.style.visibility = 'visible';
+//    if (dropDownContainer.style.visibility === 'hidden') {
+//     dropDownContainer.style.visibility = 'visible';
 
-   } 
-   else if (dropDownContainer.style.visibility = 'hidden') {
-      dropDownContainer.style.visibility = 'hidden';
+//    } 
+//    else if (dropDownContainer.style.visibility = 'hidden') {
+//       dropDownContainer.style.visibility = 'hidden';
 
-    }
+//     }
+// }
+
+// function hideMenu () {
+
+//     dropDownContainer.style.visibility = 'hidden';
+//     icon.classList.toggle("fa-times");
+//     icon.classList.toggle('fa-bars');
+
+// }
+
+const parent = document.querySelector('.modal-parent'),
+      btn = document.querySelector('button')
+      X = document.querySelector('.close-icon');
+      
+btn.addEventListener('click', show);
+
+function show() {
+  parent.style.display = 'block';
 }
-
-function hideMenu () {
-
-    dropDownContainer.style.visibility = 'hidden';
-    icon.classList.toggle("fa-times");
-    icon.classList.toggle('fa-bars');
-
-}
-
