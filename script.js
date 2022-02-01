26c9b29b7011f66d075d43e51a5620de8b2d3090
Chia Carlyle <chiacarlyle@gmail.com>
2022-02-01 14:35:18 +0100

    Adjust styling according to figma template

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 
-// let logo = document.getElementsByClassName('logo');
-// let dropDownContainer = document.getElementsByClassName('mobile-dropdown')[0];
-// let menuLinks = document.querySelectorAll('drop-option');
-// let icon = document.querySelector('.menu .fa-bars');
+let logo = document.getElementsByClassName('logo');
+let dropDownContainer = document.getElementsByClassName('mobile-dropdown')[0];
+let menuLinks = document.querySelectorAll('drop-option');
+let icon = document.querySelector('.menu .fa-bars');
 
-// dropDownContainer.style.visibility = 'hidden';
+dropDownContainer.style.visibility = 'hidden';
 
 
-//  function changeIcon() {
-//    icon.classList.toggle("fa-times");
-//    icon.classList.toggle('fa-bars');
-//    icon.style.position = 'absolute';
-//    icon.style.right = '0';
-//    icon.style.marginRight = '24px';
+ function changeIcon() {
+   icon.classList.toggle("fa-times");
+   icon.classList.toggle('fa-bars');
+   icon.style.position = 'absolute';
+   icon.style.right = '0';
+   icon.style.marginRight = '24px';
 
-//    if (dropDownContainer.style.visibility === 'hidden') {
-//     dropDownContainer.style.visibility = 'visible';
+   if (dropDownContainer.style.visibility === 'hidden') {
+    dropDownContainer.style.visibility = 'visible';
 
-//    } 
-//    else if (dropDownContainer.style.visibility = 'hidden') {
-//       dropDownContainer.style.visibility = 'hidden';
+   } 
+   else if (dropDownContainer.style.visibility = 'hidden') {
+      dropDownContainer.style.visibility = 'hidden';
 
-//     }
-// }
+    }
+}
 
-// function hideMenu () {
+function hideMenu () {
 
-//     dropDownContainer.style.visibility = 'hidden';
-//     icon.classList.toggle("fa-times");
-//     icon.classList.toggle('fa-bars');
+    dropDownContainer.style.visibility = 'hidden';
+    icon.classList.toggle("fa-times");
+    icon.classList.toggle('fa-bars');
 
-// }
+}
 
 const parent = document.querySelector('.modal-parent'),
       btn = document.querySelector('button')
